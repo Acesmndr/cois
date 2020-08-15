8924b99e64a5b03223f087150493b23bb60f562a
acesmndr <acesmndr@gmail.com>
2020-08-15 15:44:34 +0545

    Updates

---
+++ precache-manifest.d25a58a58224178d73d085850327b003.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb9a2904040d59ce6c5570d24845d5e",
+    "url": "/cois/index.html"
+  },
+  {
+    "revision": "6f326a783b90b2055281",
+    "url": "/cois/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "4258b7ba4e8db998c042",
+    "url": "/cois/static/css/main.b2644fec.chunk.css"
+  },
+  {
+    "revision": "6f326a783b90b2055281",
+    "url": "/cois/static/js/2.68e58157.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cois/static/js/2.68e58157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4258b7ba4e8db998c042",
+    "url": "/cois/static/js/main.3f386b0c.chunk.js"
+  },
+  {
+    "revision": "3d7ce40a2d5389a1d600",
+    "url": "/cois/static/js/runtime-main.c17b4a98.js"
+  },
+  {
+    "revision": "992d3132743a4218e9db60fdb90ce968",
+    "url": "/cois/static/media/4Nerds.992d3132.jpg"
+  },
+  {
+    "revision": "a1f872e39e79e7aeb162ba2bd862c6e2",
+    "url": "/cois/static/media/4Nerds.a1f872e3.png"
+  },
+  {
+    "revision": "ba24deac10b20df9920217f5735d5d3a",
+    "url": "/cois/static/media/FatalJay.ba24deac.jpg"
+  },
+  {
+    "revision": "0d878acda6aba63288324facebd8ab19",
+    "url": "/cois/static/media/FilmGob.0d878acd.png"
+  },
+  {
+    "revision": "a53b0ef1b090e6626ec72352f50f02f1",
+    "url": "/cois/static/media/FilmGob.a53b0ef1.jpg"
+  },
+  {
+    "revision": "5fcfbf9f583dd2af84251002fabb3572",
+    "url": "/cois/static/media/MadMaxMandalorian.5fcfbf9f.jpg"
+  },
+  {
+    "revision": "5b608f3fcac81775afdecd1298bf1198",
+    "url": "/cois/static/media/NerdyInManyWays.5b608f3f.png"
+  },
+  {
+    "revision": "122f215821d65de09072912741f1b9e9",
+    "url": "/cois/static/media/NickatinaShow.122f2158.png"
+  },
+  {
+    "revision": "93479f9fbf41661143c713ca909fdd21",
+    "url": "/cois/static/media/PoindexterLounge.93479f9f.jpg"
+  },
+  {
+    "revision": "044b1522c04f7bbb6e5b147a0f612769",
+    "url": "/cois/static/media/SubjectiveReviews.044b1522.jpg"
+  },
+  {
+    "revision": "942ddd33ab0f890cd10a0cfb3ca7f931",
+    "url": "/cois/static/media/SubjectiveReviews.942ddd33.png"
+  },
+  {
+    "revision": "264b1b6badb864a88f39b363f1f48975",
+    "url": "/cois/static/media/Zeeverfet.264b1b6b.jpg"
+  },
+  {
+    "revision": "c74952601b317702defd120e4d2273a6",
+    "url": "/cois/static/media/crisis.c7495260.png"
+  },
+  {
+    "revision": "9610a4de2267709e66f60a43b880afdf",
+    "url": "/cois/static/media/crisislogo.9610a4de.jpg"
+  },
+  {
+    "revision": "64a812907081e9ccea8ea1ee7fbd2c82",
+    "url": "/cois/static/media/dcfandome.64a81290.png"
+  }
+]);
